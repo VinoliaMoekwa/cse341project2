a275dfeb8e24bde11d267220195622977b9f0a36
VinoliaMoekwa <moe23001@byui.edu>
2025-02-01 21:37:44 +0200

    product.js fixed

--- routes/products.js
+++ routes/products.js
@@ -4,8 +4,8 @@ const productsController = require('../controllers/products');
 
 router.get('/', productsController.getAll);
 router.get('/:id', productsController.getSingle);
-router.post('/', productsController.createContact);
-router.put('/:id', productsController.updateContact);
-router.delete('/:id', productsController.deleteContact);
+router.post('/', productsController.createProduct);
+router.put('/:id', productsController.updateProduct);
+router.delete('/:id', productsController.deleteProduct);
 
 module.exports = router;
