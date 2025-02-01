b2abc9b843699483738097bcf52a1de7e0d1c222
VinoliaMoekwa <moe23001@byui.edu>
2025-02-01 04:46:42 +0200

    Database

--- data/database.js
+++ data/database.js
@@ -1,1 +1,35 @@
+const dotenv = require('dotenv');
+dotenv.config();
 
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+let database;
+
+const initDb = (callback) => {
+    if (database) {
+        console.log('Db is already initialized');
+        return callback(null, database);
+    }
+    MongoClient.connect(process.env.MONGODB_URL)
+        .then(client => {
+            console.log('Connected to MongoDB');
+            database = client.db(process.env.vinolia341);
+            callback(null, database);
+        })
+        .catch(err => {
+            console.error('Failed to connect to MongoDB', err);
+            callback(err);
+        });
+};
+
+const getDatabase = () => {
+    if (!database) {
+        throw Error('Db is not initialized');
+    }
+    return database;
+};
+
+module.exports = {
+    initDb,
+    getDatabase
+};
