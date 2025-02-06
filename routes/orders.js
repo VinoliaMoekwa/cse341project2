c344acf34a433c59f31a4371c649ebc14715d8ec
VinoliaMoekwa <moe23001@byui.edu>
2025-02-06 15:23:28 +0200

    temporarily removing authentication

--- routes/orders.js
+++ routes/orders.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const { getAllOrders, getSingleOrder, createOrder, updateOrder, deleteOrder } = require('../controllers/orders');
 const { validateOrder, validateId } = require('../middleware/validation');
 const errorHandler = require('../middleware/errorHandler');
-const { isAuthenticated} = require('../middleware/authenticate');
+//const { isAuthenticated} = require('../middleware/authenticate');
 
 
 router.get('/', getAllOrders);
 router.get('/:id', getSingleOrder);
-router.post('/',validateOrder,isAuthenticated , createOrder);
-router.put('/:id',validateId,validateOrder,isAuthenticated,  updateOrder);
-router.delete('/:id',validateId,validateOrder,isAuthenticated, deleteOrder);
+router.post('/',validateOrder, createOrder);//put back isAthenticated 
+router.put('/:id',validateId,validateOrder,  updateOrder);//put back isAthenticated 
+router.delete('/:id',validateId,validateOrder, deleteOrder);//put back isAthenticated 
 
 
 
--- routes/products.js
+++ routes/products.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const { getAllProducts, getSingleProduct, createProduct, updateProduct, deleteProduct } = require('../controllers/products');
 const { validateProduct, validateId } = require('../middleware/validation');
 const errorHandler = require('../middleware/errorHandler');
-const { isAuthenticated} = require('../middleware/authenticate');
+//const { isAuthenticated} = require('../middleware/authenticate');
 
 
 router.get('/', getAllProducts);
 router.get('/:id', getSingleProduct);
-router.post('/',validateProduct,isAuthenticated,  createProduct);
-router.put('/:id',validateId,validateProduct,isAuthenticated, updateProduct);
-router.delete('/:id',validateId,validateProduct,isAuthenticated, deleteProduct);
+router.post('/',validateProduct,isAuthenticated,  createProduct); //Put back isAuthenticated
+router.put('/:id',validateId,validateProduct, updateProduct);//Put back isAuthenticated
+router.delete('/:id',validateId,validateProduct, deleteProduct);//Put back isAuthenticated
 
 
 
