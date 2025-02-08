fb25e440c2b66e9eaec5cf6be0266f685b754ff9
VinoliaMoekwa <moe23001@byui.edu>
2025-02-08 02:53:58 +0200

    Put Authentication back

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
-//console.log('GITHUB_CLIENT_ID:', process.env.GITHUB_CLIENT_ID);
-//console.log('GITHUB_CLIENT_SECRET:', process.env.GITHUB_CLIENT_SECRET);
-//console.log('CALLBACK_URL:', process.env.CALLBACK_URL);
+console.log('GITHUB_CLIENT_ID:', process.env.GITHUB_CLIENT_ID);
+console.log('GITHUB_CLIENT_SECRET:', process.env.GITHUB_CLIENT_SECRET);
+console.log('CALLBACK_URL:', process.env.CALLBACK_URL);
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,11 +10,11 @@ const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const mongodb = require('./data/database');
-//const passport = require('passport');
-//const session = require('express-session');
-//const MongoStore = require('connect-mongo');
-//const GitHubStrategy = require('passport-github2').Strategy;
-//const { isAuthenticated } = require('./middleware/authenticate'); // Correct import
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const GitHubStrategy = require('passport-github2').Strategy;
+const { isAuthenticated } = require('./middleware/authenticate'); 
 const errorHandler = require('./middleware/errorHandler');
 const routes = require('./routes/index');
 
@@ -27,60 +27,60 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-// Session configuration using MongoDB as store
-//app.use(
-    //session({
-        //secret: process.env.GITHUB_CLIENT_SECRET || "secret",
-        //resave: false,
-        //saveUninitialized: false,
-        //store: MongoStore.create({
-            //mongoUrl: process.env.MONGODB_URL, // MongoDB connection string from .env
-            //ttl: 14 * 24 * 60 * 60, // Session expiration time in seconds (14 days)
-        //}),
-    //})
-//);
+//Session configuration using MongoDB as store
+app.use(
+    session({
+        secret: process.env.GITHUB_CLIENT_SECRET || "secret",
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGODB_URL, // MongoDB connection string from .env
+            ttl: 14 * 24 * 60 * 60, // Session expiration time in seconds (14 days)
+        }),
+    })
+);
 
-//app.use(passport.initialize());
-//app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Passport GitHub Strategy
-//passport.use(
-    //new GitHubStrategy(
-        //{
-            //clientID: process.env.GITHUB_CLIENT_ID,
-            //clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            //callbackURL: process.env.CALLBACK_URL,
-        //},
-        //function (accessToken, refreshToken, profile, done) {
-            //return done(null, profile);
-        //}
-    //)
-//);
+passport.use(
+    new GitHubStrategy(
+        {
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: process.env.CALLBACK_URL,
+        },
+        function (accessToken, refreshToken, profile, done) {
+            return done(null, profile);
+        }
+    )
+);
 
-// Serialize & Deserialize User
-//passport.serializeUser((user, done) => {
-    //done(null, user);
-//});
-//passport.deserializeUser((user, done) => {
-    //done(null, user);
-//});
+//Serialize & Deserialize User
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
 
 // Swagger Docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Authentication Routes
-//app.get('/', (req, res) =>
-    //res.send(req.session.user ? `Logged in as ${req.session.user.displayName}` : "Logged Out")
-//);
+app.get('/', (req, res) =>
+    res.send(req.session.user ? `Logged in as ${req.session.user.displayName}` : "Logged Out")
+);
 
-//app.get(
-    //'/github/callback',
-    //passport.authenticate('github', { failureRedirect: '/api-docs', session: true }),
-    //(req, res) => {
-        //req.session.user = req.user; // Store user in session
-        //res.redirect('/');
-    //}
-//);
+app.get(
+    '/github/callback',
+    passport.authenticate('github', { failureRedirect: '/api-docs', session: true }),
+    (req, res) => {
+        req.session.user = req.user; // Store user in session
+        res.redirect('/');
+    }
+);
 
 // CORS Headers
 app.use((req, res, next) => {
@@ -91,9 +91,9 @@ app.use((req, res, next) => {
 });
 
 // Protected Route Example
-//app.get('/protected', isAuthenticated, (req, res) => {
-    //res.json({ message: "You have access to this protected route", user: req.session.user });
-//});
+app.get('/protected', isAuthenticated, (req, res) => {
+    res.json({ message: "You have access to this protected route", user: req.session.user });
+});
 
 // Routes
 app.use('/', routes);
